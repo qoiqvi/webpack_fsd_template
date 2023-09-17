b63b62d76eb98fc76f6c6c27513ebb7fd4790c19
qoiqvi <111002830+qoiqvi@users.noreply.github.com>
2023-09-17 17:50:03 +0300

    upd

--- src/features/fetchTariffsByType/fetchTariffsByType.ts
+++ src/features/fetchTariffsByType/fetchTariffsByType.ts
@@ -1,12 +1,15 @@
-import axios from "axios"
 import { Api } from "shared/api/api"
 import { TariffType } from "shared/types"
 
 export const fetchTariffsByType = async (type: TariffType | undefined) => {
-	const response = await axios.get("http://127.0.0.1:5000/tariffs", {
-		params: {
-			type: type,
-		},
-	})
-	return response
+	try {
+		const response = await Api.get("/tariffs", {
+			params: {
+				type: type,
+			},
+		})
+		return response
+	} catch (error) {
+		console.error(error)
+	}
 }
